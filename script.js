e089a93ca68d97692e2a8473431ee008350be097
LucBUTGH <lucleveque14@outlook.fr>
2023-09-11 19:05:16 +0200

    Fix accessibilities to WCAG and W3C standards

--- script.js
+++ script.js
@@ -1,119 +1,120 @@
-// Variables globales pour le compteur et le minuteur
+// Global variables for the counter and timer
 let timer;
 let isRunning = false;
 let work = true;
-let workTime = 25 * 60; //Pomoro default time in seconds
-let breakTime = 5 * 60; //Pomoro default break time in seconds
-let currentTime = workTime; //Current time (intialized at work time)
-let workElement = document.getElementById("work")
-let breakElement = document.getElementById("break")
-let playButton = document.getElementById("play")
-let resetButton = document.getElementById("reset")
+let workTime = 25 * 60; // Default work time in seconds
+let breakTime = 5 * 60; // Default break time in seconds
+let currentTime = workTime; // Current time (initialized to work time)
+let workElement = document.getElementById("work");
+let breakElement = document.getElementById("break");
+let playButton = document.getElementById("play");
+let resetButton = document.getElementById("reset");
 resetButton.style.display = "none";
 
-//Function to color current period's text (work or break)
-function CSS(){
-    if(work){
-      breakElement.style.color = "white";
-      workElement.style.color = "lightgreen";
-    }else{
-      workElement.style.color = "white";
-      breakElement.style.color = "lightgreen";
-    }
+// Function to update the text color of the current period (work or break)
+function updateTextColor() {
+  if (work) {
+    breakElement.style.color = "white";
+    workElement.style.color = "lightgreen";
+  } else {
+    workElement.style.color = "white";
+    breakElement.style.color = "lightgreen";
+  }
 }
 
-
 // Function to update the timer display
-function updateTimer() {
+function updateTimerDisplay() {
   const timerDisplay = document.getElementById("timer");
   const minutes = Math.floor(currentTime / 60);
   const seconds = currentTime % 60;
   timerDisplay.textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
-
-
 // Function to start/stop the timer
 function startTimer() {
-  resetButton.style.display = "inline"
+  resetButton.style.display = "inline";
   playButton.style.display = "none";
-    document.getElementById("form").style.display = "none";
-    if (!isRunning) {
-        timer = setInterval(function () {
-        if (currentTime <= 0) {
-            clearInterval(timer);
-            isRunning = false
-            work = false;
-            CSS();
-            startCountdown(); //Start the countdown
-        } else {
-            currentTime--;
-            updateTimer();
-        }
-      }, 1000);
-      isRunning = true;
-    } else {
-      clearInterval(timer);
-      isRunning = false;
-    }
-  }
-
-//Function to reset the timer
-function reset() {
-  playButton.style.display = "inline"
-  resetButton.style.display = "none";
+  document.getElementById("form").style.display = "none";
+  if (!isRunning) {
+    timer = setInterval(function () {
+      if (currentTime <= 0) {
+        clearInterval(timer);
+        isRunning = false;
+        work = !work; // Toggle work/break mode
+        updateTextColor();
+        startCountdown();
+      } else {
+        currentTime--;
+        updateTimerDisplay();
+      }
+    }, 1000);
+    isRunning = true;
+  } else {
     clearInterval(timer);
     isRunning = false;
-    currentTime = workTime;
-    updateTimer();
-    document.getElementById("form").style.display = "none";
+  }
 }
 
-//Function to set up the work time. The break time will be calculated according to work time. (20% of work time)
+// Function to reset the timer
+function reset() {
+  playButton.style.display = "inline";
+  resetButton.style.display = "none";
+  clearInterval(timer);
+  isRunning = false;
+  currentTime = workTime;
+  updateTimerDisplay();
+  document.getElementById("form").style.display = "none";
+}
+
+// Function to configure the timer
 function configure() {
   console.log("OK");
-    reset();
-    const timeInput = document.getElementById("time");
-    timeInput.value = currentTime;
-    document.getElementById("form").style.display = "block";
+  reset();
+  const timeInput = document.getElementById("worktime");
+  const breaktimeInput = document.getElementById("breaktime");
+  timeInput.value = currentTime;
+  breaktimeInput.value = breakTime;
+  document.getElementById("form").style.display = "block";
 }
 
-//Function to save the configuration
+// Function to update the timer configuration
 function updateConfigure() {
-  const timeInput = document.getElementById("time");
+  const timeInput = document.getElementById("worktime");
+  const breaktimeInput = document.getElementById("breaktime");
   const newTime = parseInt(timeInput.value, 10);
-  if (!isNaN(newTime) && newTime > 0) {
+  const newBreakTime = parseInt(breaktimeInput.value, 10);
+  if (!isNaN(newTime) && newTime > 0 && !isNaN(newBreakTime) && newBreakTime > 0) {
     currentTime = newTime;
     workTime = newTime;
-    breakTime = newTime;
-    updateTimer();
+    breakTime = newBreakTime;
+    updateTimerDisplay();
     document.getElementById("form").style.display = "none";
   }
 }
 
-//Function to start the break timer
+// Function to start the break countdown timer
 function startCountdown() {
-    if (!isRunning) {
-        const countdownTime = Math.floor(workTime / 5); //20% work time
-        currentTime = countdownTime; //Update the current time with the countdown
-        updateTimer();
-        timer = setInterval(function () {
-        if (currentTime <= 0) {
-            clearInterval(timer);
-            isRunning = false;
-            work = true; // Back to work mode
-            CSS();
-            currentTime = workTime;
-            updateTimer();
-            startTimer(); //Start the main timer
-        } else {
-            currentTime--;
-            updateTimer();
-        }
-      }, 1000);  
-      isRunning = true;
-    }
+  if (!isRunning) {
+    const countdownTime = breakTime;
+    currentTime = countdownTime;
+    updateTimerDisplay();
+    timer = setInterval(function () {
+      if (currentTime <= 0) {
+        clearInterval(timer);
+        isRunning = false;
+        work = true; // Return to work mode
+        updateTextColor();
+        currentTime = workTime;
+        updateTimerDisplay();
+        startTimer(); // Start the main timer
+      } else {
+        currentTime--;
+        updateTimerDisplay();
+      }
+    }, 1000);
+    isRunning = true;
   }
+}
 
-updateTimer();
-CSS();
+updateTimerDisplay();
+updateTextColor();
