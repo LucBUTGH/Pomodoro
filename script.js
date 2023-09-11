91c88bb4781292938fa131023945802c837fac68
LucBUTGH <lucleveque14@outlook.fr>
2023-09-11 16:05:05 +0200

    Commit some fixes

--- script.js
+++ script.js
@@ -7,14 +7,19 @@ let breakTime = 5 * 60; // 5 minutes en secondes
 let currentTime = workTime; // Temps actuel (initialisé au temps de travail par défaut)
 let workElement = document.getElementById("work")
 let breakElement = document.getElementById("break")
+let playButton = document.getElementById("play")
+let resetButton = document.getElementById("reset")
+resetButton.style.display = "none";
+
 
 function CSS(){
-  if(work){
-    workElement.style.color = "lightgreen";
-  }else{
-      breakElement.style.color = "lightgreen";
-  }
+    if(work){
+      workElement.style.color = "lightgreen";
+    }else{
+        breakElement.style.color = "lightgreen";
+    }
 }
+
 
 // Fonction pour mettre à jour l'affichage du minuteur
 function updateTimer() {
@@ -24,18 +29,12 @@ function updateTimer() {
   timerDisplay.textContent = `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
-//Réinitialiser minuteur
-function reset() {
-  clearInterval(timer);
-  isRunning = false;
-  currentTime = workTime;
-  updateTimer();
-  document.getElementById("form").style.display = "none";
-}
 
 
 // Fonction pour démarrer/arrêter le minuteur
 function startTimer() {
+  resetButton.style.display = "inline"
+  playButton.style.display = "none";
     document.getElementById("form").style.display = "none";
     if (!isRunning) {
         timer = setInterval(function () {
@@ -57,28 +56,40 @@ function startTimer() {
     }
   }
 
-  function configure() {
-    console.log("OK");
-      reset();
-      const timeInput = document.getElementById("time");
-      timeInput.value = currentTime;
-      document.getElementById("form").style.display = "block";
-  }
-  
-  // Fonction pour mettre à jour la configuration
-  function updateConfigure() {
-    const timeInput = document.getElementById("time");
-    const newTime = parseInt(timeInput.value, 10);
-    if (!isNaN(newTime) && newTime > 0) {
-      currentTime = newTime;
-      workTime = newTime;
-      breakTime = newTime;
-      updateTimer();
-      document.getElementById("form").style.display = "none";
-    }
-  }
+// Fonction pour réinitialiser le minuteur
+function reset() {
+  playButton.style.display = "inline"
+  resetButton.style.display = "none";
+    clearInterval(timer);
+    isRunning = false;
+    currentTime = workTime;
+    updateTimer();
+    document.getElementById("form").style.display = "none";
+}
 
-  function startCountdown() {
+// Fonction pour configurer le temps de travail et de pause
+function configure() {
+  console.log("OK");
+    reset();
+    const timeInput = document.getElementById("time");
+    timeInput.value = currentTime;
+    document.getElementById("form").style.display = "block";
+}
+
+// Fonction pour mettre à jour la configuration
+function updateConfigure() {
+  const timeInput = document.getElementById("time");
+  const newTime = parseInt(timeInput.value, 10);
+  if (!isNaN(newTime) && newTime > 0) {
+    currentTime = newTime;
+    workTime = newTime;
+    breakTime = newTime;
+    updateTimer();
+    document.getElementById("form").style.display = "none";
+  }
+}
+
+function startCountdown() {
     if (!isRunning) {
         const countdownTime = Math.floor(workTime / 5); // 1/5 du temps de travail
         currentTime = countdownTime; // Mettre à jour le temps actuel avec le countdown
@@ -100,9 +111,6 @@ function startTimer() {
       isRunning = true;
     }
   }
-
-
-
 
 
 updateTimer();
